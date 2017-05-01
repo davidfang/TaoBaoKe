7a4dafe17760def5d19221aa7e5a2ecb7f22229a
zq <qianzgcn@163.com>
2017-05-01 21:58:15 +0800

    修改模态框点击登录函数

--- src/component/top/ModalBox.js
+++ src/component/top/ModalBox.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 export default class ModalBox extends React.Component{
+    constructor(){
+        super();
+    }
+
+    handleLogin(){
+        this.props.onClick();
+    }
     render() {
         return (
-            <div className="modalBox">
-                <input placeholder="账户:"/>
-                <input placeholder="密码:" />
-                <button onClick={() => this.props.onClick()}>立即登录</button>
+            <div className="modalBg">
+                <div className="modalBox">
+                    <input placeholder="账户:"/>
+                    <input placeholder="密码:" type="password"/>
+                    <button onClick={this.handleLogin.bind(this)}>立即登录</button>
+                </div>
             </div>
         )
     }
