7327367fc33aeed7905b80813cd57d2e6097b83e
zq <qianzgcn@163.com>
2017-05-01 21:57:33 +0800

    修改登录后的逻辑

--- src/component/top/Login.js
+++ src/component/top/Login.js
@@ -5,6 +5,7 @@ export default class Login extends React.Component{
         super();
         this.state = {
            ModalIsShow : false,
+            userName:"会员登录"
         }
     }
     handleLogin(){  //弹出模态框
@@ -12,14 +13,17 @@ export default class Login extends React.Component{
             ModalIsShow:true,
         })
     }
-    getInformation(){
 
-    }
     handCloseModal(){ //关闭模态框
         this.setState({
             ModalIsShow:!this.state.ModalIsShow,
         });
-        this.getInformation();
+        //if(是否输入正确账户密码)
+        {   this.setState({
+                userName:"某某用户"
+            })
+            this.handleLogin=function () {};//登录成功后注销登录函数
+        }
     }
     renderModal(){
         return this.state.ModalIsShow ? <ModalBox ModalStatus={this.state.ModalIsShow} onClick={()=>this.handCloseModal()}/> : null;
@@ -28,7 +32,7 @@ export default class Login extends React.Component{
     render() {
         return (
             <div className="login">
-                <span onClick={this.handleLogin.bind(this)}>会员登录</span>
+                <span onClick={this.handleLogin.bind(this)}>{this.state.userName}</span>
                 <div>
                     {this.renderModal()}
                 </div>
